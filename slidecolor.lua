78af94662c73ff265f1a2826a270377f4657d7f4
Wowo Diergo Suciawo <wowo.diergo@gmail.com>
2018-02-12 17:55:56 +0700

    Randomizing grid before enter play scene

--- slidecolor.lua
+++ slidecolor.lua
@@ -13,20 +13,22 @@ MOVE_UP = "UP"
 MOVE_DOWN = "DOWN"
 MOVE_LEFT = "LEFT"
 MOVE_RIGHT = "RIGHT"
+START_WIDTH = print("START", 0, 0)
 
 function init()
   scene = SCENE_MENU
+
   --init variables for menu
   colors = {
-    { r = 25,  g = 25,  b = 25,  isChanged = true  }, -- 1
+    { r = 55,  g = 55,  b = 55,  isChanged = true  }, -- 1
     { r = 68,  g = 36,  b = 52,  isChanged = false }, -- 2
-    { r = 200, g = 75,  b = 100, isChanged = true  }, -- 3
+    { r = 215, g = 115, b = 135, isChanged = true  }, -- 3
     { r = 78,  g = 74,  b = 79,  isChanged = false }, -- 4
-    { r = 8,   g = 8,   b = 8,   isChanged = true  }, -- 5
+    { r = 10,  g = 10,  b = 10,  isChanged = true  }, -- 5
     { r = 52,  g = 101, b = 36,  isChanged = false }, -- 6
-    { r = 25,  g = 75,  b = 225, isChanged = true  }, -- 7
+    { r = 75,  g = 115, b = 235, isChanged = true  }, -- 7
     { r = 117, g = 113, b = 97,  isChanged = false }, -- 8
-    { r = 50,  g = 175, b = 50,  isChanged = true  }, -- 9
+    { r = 95,  g = 195, b = 95,  isChanged = true  }, -- 9
     { r = 210, g = 125, b = 44,  isChanged = false }, -- 10
     { r = 133, g = 149, b = 161, isChanged = false }, -- 11
     { r = 109, g = 170, b = 44,  isChanged = false }, -- 12
@@ -43,6 +45,18 @@ function init()
     { color = 9, rStep = 2, gStep = 7, bStep = 2 }
   }
   cursorPos = 1
+  menuMessage = {
+    x = 0,
+    y = 117,
+    width = 0,
+    text = "Menu Message",
+    prevText = "Menu Message",
+    duration = 90, --2s
+    timer = 0
+  }
+  menuMessage.width = print(menuMessage.text, 0, 0)
+  menuMessage.x = (240 - menuMessage.width) // 2
+
   --init variables for playing
   grid = {
     { 1, 2, 3 },
@@ -71,11 +85,19 @@ end
 
 function updateMenu()
   --moving cursor up and down
-  if btnp(0, 0, 30) and cursorPos > 1 then
-    cursorPos = cursorPos - 1
+  if btnp(0, 0, 30) then
+    if cursorPos > 1 then
+      cursorPos = cursorPos - 1
+    else
+      cursorPos = 13
+    end
   end
-  if btnp(1, 0, 30) and cursorPos < 13 then
-    cursorPos = cursorPos + 1
+  if btnp(1, 0, 30) then
+    if cursorPos < 13 then
+      cursorPos = cursorPos + 1
+    else
+      cursorPos = 1
+    end
   end
   --changing value by pressing left and right
   --or start the game if cursor on start label
@@ -101,8 +123,8 @@ function updateMenu()
       end
     end
   elseif btnp(2, 0, 30) or btnp(3, 0, 30) then
-    generateColor()
-    scene = SCENE_PLAY
+    --preparing to go to SCENE_PLAY
+    goToScenePlay()
   end
   --changing color on memory
   if (anyChanges) then
@@ -120,7 +142,7 @@ function drawMenu()
   --draw menu options
   rect(15, 10, 30, 30, 1)
 
-  print("R : "..menuValues[1].rStep, 60, 10, 15)
+  print("R : "..menuValues[1]["rStep"], 60, 10, 15)
   print("G : "..menuValues[1]["gStep"], 60, 25, 15)
   print("B : "..menuValues[1]["bStep"], 60, 40, 15)
   rect(15, 70, 30, 30, 3)
@@ -137,7 +159,15 @@ function drawMenu()
   print("G : "..menuValues[4]["gStep"], 180, 85, 15)
   print("B : "..menuValues[4]["bStep"], 180, 100, 15)
 
-  print("START?", 120, 125, 15)
+  print("START", (240-START_WIDTH)//2, 125, 15)
+  if menuMessage.timer > 0 then
+    --re-calculate text width if text changed
+    if menuMessage.text ~= menuMessage.prevText then
+      menuMessage.width = print(menuMessage.text, 0, 0)
+    end
+    print(menuMessage.text, menuMessage.x, menuMessage.y)
+    menuMessage.timer = menuMessage.timer - 1
+  end
 
   --draw cursor
   if cursorPos <= 3 then
@@ -149,19 +179,26 @@ function drawMenu()
   elseif cursorPos <= 12 then
     spr(1, 170, (cursorPos-6) * 15 + 9)
   else
-    spr(1, 110, 124)
+    spr(1, (240-START_WIDTH)/2-10, 124)
   end
+end
+
+--preparing to go to SCENE_PLAY
+function goToScenePlay()
+  generateColor()
+  randomizeGrid()
+  scene = SCENE_PLAY
 end
 
 --function to changing color based on input
 function changeColor(index, color, delta)
   --increase colors value
   if color == COLOR_RED then
-    colors[index]["r"] = colors[index]["r"] + (delta * 25)
+    colors[index]["r"] = colors[index]["r"] + (delta * 20)
   elseif color == COLOR_GREEN then
-    colors[index]["g"] = colors[index]["g"] + (delta * 25)
+    colors[index]["g"] = colors[index]["g"] + (delta * 20)
   elseif color == COLOR_BLUE then
-    colors[index]["b"] = colors[index]["b"] + (delta * 25)
+    colors[index]["b"] = colors[index]["b"] + (delta * 20)
   end
   colors[index]["isChanged"] = true
   anyChanges = true
@@ -173,13 +210,14 @@ function generateColor()
   calculateColor(4, 1, 7)
   calculateColor(6, 3, 9)
   calculateColor(8, 7, 9)
-  --for background color on playing scene
-  calculateColor(5, 5, 2)
-  calculateColor(5, 5, 4)
-  calculateColor(5, 5, 6)
-  calculateColor(5, 5, 8)
+  ----for background color on playing scene
+  --calculateColor(5, 5, 2)
+  --calculateColor(5, 5, 4)
+  --calculateColor(5, 5, 6)
+  --calculateColor(5, 5, 8)
   anyChanges = true
 end
+
 --function to calculate intermediate color
 function calculateColor(target, indexA, indexB)
   colors[target]["r"] = (colors[indexA]["r"] + colors[indexB]["r"]) / 2
@@ -187,6 +225,8 @@ function calculateColor(target, indexA, indexB)
   colors[target]["b"] = (colors[indexA]["b"] + colors[indexB]["b"]) / 2
   colors[target]["isChanged"] = true
 end
+
+--https://github.com/nesbox/TIC-80/wiki/code-examples-and-snippets#pal-function
 --sets the palette indice i to specified rgb
 --or return the colors if no rgb values are declared.
 function pal(i,r,g,b)
@@ -209,19 +249,93 @@ function pal(i,r,g,b)
   end
 end
 
+--function to randomize the grid
+function randomizeGrid()
+  local randomStep = math.random(20, 50)
+  local lastStep
+  local currentStep
+  local i = 0
+  while(i < randomStep) do
+    local step = math.random(80) % 4
+    currentStep = (step == 0) and MOVE_UP or 
+                  (step == 1) and MOVE_DOWN or
+                  (step == 2) and MOVE_LEFT or
+                  (step == 3) and MOVE_RIGHT
+    --skip if current step is canceling last step
+    --also check if move is valid
+    if currentStep ~= lastStep and isValidMove(currentStep) then
+      slideGrid(currentStep)
+      lastStep = oppositeMove(currentStep)
+      i = i + 1
+    end
+  end
+end
+
+function isValidMove(move)
+  if move == MOVE_UP and emptyGrid.x < 3 then
+    return true
+  elseif move == MOVE_DOWN and emptyGrid.x > 1 then
+    return true
+  elseif move == MOVE_LEFT and emptyGrid.y < 3 then
+    return true
+  elseif move == MOVE_RIGHT and emptyGrid.y > 1 then
+    return true
+  else
+    return false
+  end
+end
+
+function oppositeMove(move)
+  if move == MOVE_UP then
+    return MOVE_DOWN
+  elseif move == MOVE_DOWN then
+    return MOVE_UP
+  elseif move == MOVE_LEFT then
+    return MOVE_RIGHT
+  elseif move == MOVE_RIGHT then
+    return MOVE_LEFT
+  else
+    return nil
+  end
+end
+
+function slideGrid(move)
+  if move == MOVE_UP then
+    grid[emptyGrid.x][emptyGrid.y] = grid[emptyGrid.x + 1][emptyGrid.y]
+    grid[emptyGrid.x + 1][emptyGrid.y] = 0
+    emptyGrid.x = emptyGrid.x + 1
+  elseif move == MOVE_DOWN then
+    grid[emptyGrid.x][emptyGrid.y] = grid[emptyGrid.x - 1][emptyGrid.y]
+    grid[emptyGrid.x - 1][emptyGrid.y] = 0
+    emptyGrid.x = emptyGrid.x - 1
+  elseif move == MOVE_LEFT then
+    grid[emptyGrid.x][emptyGrid.y] = grid[emptyGrid.x][emptyGrid.y + 1]
+    grid[emptyGrid.x][emptyGrid.y + 1] = 0
+    emptyGrid.y = emptyGrid.y + 1
+  elseif move == MOVE_RIGHT then
+    grid[emptyGrid.x][emptyGrid.y] = grid[emptyGrid.x][emptyGrid.y - 1]
+    grid[emptyGrid.x][emptyGrid.y - 1] = 0
+    emptyGrid.y = emptyGrid.y - 1
+  end
+end
+
 function updatePlay()
   --control where to slide
   if btnp(0, 0, 30) then
     --up button
+    if isValidMove(MOVE_UP) then slideGrid(MOVE_UP) end
   end
   if btnp(1, 0, 30) then
     --down button
+    if isValidMove(MOVE_DOWN) then slideGrid(MOVE_DOWN) end
   end
   if btnp(2, 0, 30) then
     --left button
+    if isValidMove(MOVE_LEFT) then slideGrid(MOVE_LEFT) end
   end
   if btnp(3, 0, 30) then
     --right button
+    if isValidMove(MOVE_RIGHT) then slideGrid(MOVE_RIGHT) end
   end
 end
 
@@ -230,7 +344,7 @@ function drawPlay()
   for i,row in pairs(grid) do
     for j, val in pairs(row) do
       if val > 0 then
-        rect(((4*j)-1)*8, ((4*i)-1)*8, 32, 32, val)
+        rect(((4*j)-1)*8, ((4*i)-1)*8, 32, 32, val)        
       end
     end
   end
